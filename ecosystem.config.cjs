a0d80a3f278fd7c5d6fa76a693bf72d90eeddd55
BaaskaDev <baasandorj.ja@gmail.com>
2026-04-07 06:01:44 +0800

    ecosystem update

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -16,11 +16,12 @@ module.exports = {
       user: "strapi",
       host: "34.92.72.76",
       key: "~/.ssh/id_ed25519",
-      "pre-setup": "echo 'pre-setup ok'",
+      ssh_options: 'StrictHostKeyChecking=no',
       env: {
         NVM_DIR: "/home/strapi/.nvm",
         PATH: "/home/strapi/.nvm/versions/node/v20.20.2/bin:/usr/bin:/bin"
       },
+      "pre-setup": "echo 'pre-setup ok'",
       ref: "origin/main",
       repo: "git@github.com:Zvkkaaa/sportzone-hub.git",
       path: "/opt/sportzone-hub",
