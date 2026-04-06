9421ef3440adab48868280758fba8ae1d8727f58
Baasandorj Janchiv <101308370+BaaskaDev@users.noreply.github.com>
2026-04-07 00:42:23 +0800

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,31 +1,35 @@
 module.exports = {
-  apps: [{
-    name: 'sportzone’,
-    script: 'npm run start:prod',
-    instances: 1,
-    autorestart: true,
-    watch: false,
-    max_memory_restart: '500M'
-  }],
+  apps: [
+    {
+      name: "sportzone",
+      script: "npm",
+      args: "run start:prod",
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "500M",
+    },
+  ],
 
   deploy: {
     dev: {
-      user: 'strapi',
-      host: '35.220.201.97',
-      ref: 'origin/main',
-      path: '/opt/sportzone-hub',
-      repo: 'git@github.com:Zvkkaaa/sportzone-hub.git',
-      'post-deploy': 'npm install && rm -rf dist && npm run build && pm2 reload ecosystem.config.js',
-      'pre-setup': ''
+      user: "strapi",
+      host: "35.220.201.97",
+      ref: "origin/main",
+      repo: "git@github.com:Zvkkaaa/sportzone-hub.git",
+      path: "/opt/sportzone-hub",
+      "post-deploy":
+        "npm install && rm -rf dist && npm run build && pm2 reload ecosystem.config.js",
     },
+
     prod: {
-      user: 'strapi',
-      host: '35.220.201.97',
-      ref: 'origin/main',
-      path: '/opt/sportzone-hub',
-      repo: 'git@github.com:Zvkkaaa/sportzone-hub.git',
-      'post-deploy': 'npm install && rm -rf dist && npm run build && pm2 reload ecosystem.config.js',
-      'pre-setup': ''
-    }
-  }
-}
+      user: "strapi",
+      host: "35.220.201.97",
+      ref: "origin/main",
+      repo: "git@github.com:Zvkkaaa/sportzone-hub.git",
+      path: "/opt/sportzone-hub",
+      "post-deploy":
+        "npm install && rm -rf dist && npm run build && pm2 reload ecosystem.config.js",
+    },
+  },
+};
